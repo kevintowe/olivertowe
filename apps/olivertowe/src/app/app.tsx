bb90ccb027dd8ef6bb4fa4ef009f91cbfdb29c01
Kevin Towe <29900411+kevintowe@users.noreply.github.com>
2023-01-29 09:49:28 -0600

    added harry potter puppet pals embedded youtube

--- apps/olivertowe/src/app/app.tsx
+++ apps/olivertowe/src/app/app.tsx
@@ -1,13 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="olivertowe" />
-      <div />
-    </>
+    <iframe
+      width="1280"
+      height="853"
+      src="https://www.youtube.com/embed/Tx1XIm6q4r4"
+      title="Potter Puppet Pals: The Mysterious Ticking Noise"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    ></iframe>
   );
 }
 
